47c59fdb6a351638da1d3a4e56281feff813051e
Here East <nastiapiven@gmail.com>
2021-03-10 20:04:18 +0100

    feat: correct min, max and average problems

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 
 exports.min = function min (array) {
-  return Math.min(...array);
+  if (array == undefined || array.length < 1) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return Math.max(...array);
+  if (array == undefined || array.length < 1) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
-let sum = 0;
 exports.avg = function avg (array) {
+  if (array == undefined || array.length < 1) {
+    return 0;
+  }
+
+  let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum = sum + array[i];
   }
-  return sum/array.length;
+
+  return sum / array.length;
 }
