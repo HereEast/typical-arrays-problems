bfd2ff6e0e848ffa781836ea6506f9d19e3f2042
Here East <nastiapiven@gmail.com>
2021-03-10 18:21:21 +0100

    feat: code min and max in the arrays

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.min(...array);
 }
 
 exports.avg = function avg (array) {
