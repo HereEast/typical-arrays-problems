bac2825ff4c84527e12f333426370033acf3c0c3
Here East <nastiapiven@gmail.com>
2021-03-10 19:05:25 +0100

    feat: add average calculation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ exports.max = function max (array) {
   return Math.max(...array);
 }
 
+let sum = 0;
 exports.avg = function avg (array) {
-  return 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum/array.length;
 }
